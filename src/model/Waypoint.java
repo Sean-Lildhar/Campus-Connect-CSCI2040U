e0815640a578a4522ed7e5e4a6ecf9e8a6da7455
Sean-Lildhar <sean.lildhar2000@gmail.com>
2026-03-28 17:10:14 -0400

    Nav working between SCI LAB and SCI LECTURE

--- src/model/Waypoint.java
+++ src/model/Waypoint.java
@@ -5,16 +5,19 @@ public class Waypoint {
     private final String description;
     private final String building;
     private final int floor;
+    private final String locationType;
 
-    public Waypoint(String id, String description, String building, int floor) {
+    public Waypoint(String id, String description, String building, int floor, String locationType) {
         this.id = id;
         this.description = description;
         this.building = building;
         this.floor = floor;
+        this.locationType =  locationType;
     }
 
     public String getId() { return id; }
     public String getDescription() { return description; }
     public String getBuilding() { return building; }
     public int getFloor() { return floor; }
+    public String getLocationType() { return locationType; }
 }
