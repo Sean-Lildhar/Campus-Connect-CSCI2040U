aa8d2d69b05664ad1c5105cf47f07332d56d62f5
kabi <kabishan.sutharman@ontariotechu.net>
2026-04-03 14:30:31 -0400

    Simple Review System

--- src/data/EditReviews.java
+++ src/data/EditReviews.java
@@ -1,5 +1,35 @@
 package data;
 
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditReviews {
+    private final String filepath = "data/review.csv";
+
+    public void addReview(String user, String room, int rating, String text){
+        try(PrintWriter pw = new PrintWriter(new FileWriter(filepath, true))){
+            pw.println(user + "," + room + "," + rating + "," + text);
+        } catch (IOException e){
+            System.err.println("Error leaving a review");
+        }
+    }
+
+    public List<String> getRoomReviews(String room){
+        List<String> reviews = new ArrayList<>();
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath))){
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                String cells[] = line.split(",", 4);
+                if(cells[1].equals(room)){
+                    reviews.add(cells[0] + " (" + cells[2] + " Star(s)): " + cells[3]);
+                }
+            }
+        } catch(IOException e){
+            System.err.println("Error loading reviews");
+        }
+        return reviews;
+    }
 }
--- src/gui/ReviewFrame.java
+++ src/gui/ReviewFrame.java
@@ -5,17 +5,27 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.*;
 import java.util.Hashtable;
+import data.EditReviews;
 
 public class ReviewFrame extends JFrame{
     private JTextArea textAreaReview;
     private JSlider starSlider;
+    private final String user;
+    private final String roomNumber;
 
-    public void ReviewFrame(String roomNumber, String user) {
-        this.setTitle("Review System");
+    public ReviewFrame(String roomNumber, String user) {
+        this.roomNumber = roomNumber;
+        this.user = user;
+
+        this.setTitle("Review System: " + roomNumber);
         this.setLayout(new BorderLayout());
         this.setSize(400, 400);
         this.setLocationRelativeTo(null);
 
+        reviewUI();
+    }
+
+    private void reviewUI(){
         // Create slider (min=0, max=10, initial=7 to represent "Normal" area)
         starSlider = new JSlider(JSlider.HORIZONTAL, 0, 5, 0);
 
@@ -38,7 +48,16 @@ public class ReviewFrame extends JFrame{
 
         JPanel submitPanel = new JPanel();
         JButton submitButton = new JButton("Submit Review");
-        //submitButton.addActionListener(e -> storeReview(this.getRoomNumber()));
+        submitButton.addActionListener(e -> {
+            int rating = starSlider.getValue();
+            String text = textAreaReview.getText().trim();
+
+            EditReviews editReview = new EditReviews();
+            editReview.addReview(user, roomNumber, rating, text);
+            JOptionPane.showMessageDialog(this, "Review Submitted", "Review", JOptionPane.INFORMATION_MESSAGE);
+            dispose();
+            //storeReview(this.getRoomNumber())
+        });
         submitPanel.add(submitButton);
 
         this.add(starSlider, BorderLayout.NORTH);
